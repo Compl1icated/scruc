89304973123dbb36ff0076155f1961b5e49c6c73
Compl1icated <76661540+Compl1icated@users.noreply.github.com>
2021-01-06 16:07:55 +0200

    Add files via upload

---
+++ a.lua
@@ -0,0 +1,4 @@
+getgenv().Color = Color3.fromRGB(255, 105, 195) --default color
+--Note if you put something which isn't a color it will automatically use the color provided above
+
+loadstring(game:HttpGet("https://pastebin.com/raw/zbkxXDnU"))()
